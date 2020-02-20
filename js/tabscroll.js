100a6d21a1b77483cf059ac9dac5a22d0dd9122a
hsx <xahesx111@outlook.com>
2020-02-20 16:35:47 +0800

    onscroll时使用函数节流

--- js/tabscroll.js
+++ js/tabscroll.js
@@ -83,9 +83,20 @@ TabScroll.prototype.init = function () {
     this.scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
 
     // 监听window的滚动事件，赋值给当前的滚动条位置
+    // 定义一个flag，用作后面判断目标函数执行
+    let flag = true;
     window.onscroll = function () {
         _this.scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-        _this.scrollTab();
+        
+        // 如果标记为false，表示目标函数还没有执行完毕，直接reuturn
+        if (!flag) return;
+        // 直接将flag重置为 false
+        flag = false;
+        // 定时器里跑目标函数，并在函数执行完毕以后重置 flag 为 true,以确保下次函数能正常执行
+        setTimeout(() => {
+          _this.scrollTab(_this);
+          flag = true;
+        }, 300)
     }
     // 给tab添加点击事件
     for (let i=0;i < this.length; i++) {
@@ -103,74 +114,75 @@ TabScroll.prototype.init = function () {
  * @method scrollTab 滚动切换
  * 
  * */ 
-TabScroll.prototype.scrollTab = function () {
+TabScroll.prototype.scrollTab = function (_this) {
+    console.log(1);
     // 从页面顶端到第二个位置的距离
-    this.activeTab = this.areaArr[1].offsetTop - this.fixdTabHeight;
+    _this.activeTab = _this.areaArr[1].offsetTop - _this.fixdTabHeight;
 
     // 当页面没有或者之后一个tab的时候返回
-    if (this.length === 0 || this.length === 1) {
+    if (_this.length === 0 || _this.length === 1) {
         return false;
     };
     
     // 当页面从头到第一个tab位置的距离大于当前滚动条的位置并且滚动条位置不为0时，给tab的第一项加选中效果的class
-    if (this.activeTab > this.scrollTop && this.scrollTop > 0) {
+    if (_this.activeTab > _this.scrollTop && _this.scrollTop > 0) {
 
         // 先获取原来类的className
-        this.oldClassName = this.eletabArr[0].getAttribute('class');
+        _this.oldClassName = _this.eletabArr[0].getAttribute('class');
 
         // 如果之前的类里有选中项的类，则不添加这个类
-        if (this.oldClassName.indexOf(this.activeClass) != -1) {
-            this.eletabArr[0].setAttribute('class', this.oldClassName);
+        if (_this.oldClassName.indexOf(_this.activeClass) != -1) {
+            _this.eletabArr[0].setAttribute('class', _this.oldClassName);
         } else {
             // 将原来的类和选中的类做拼接
-            this.newClassName = this.oldClassName + " " + this.activeClass;
+            _this.newClassName = _this.oldClassName + " " + _this.activeClass;
 
             // 最后将拼接到的新类赋值给当前的tab
-            this.eletabArr[0].setAttribute('class', this.newClassName);
+            _this.eletabArr[0].setAttribute('class', _this.newClassName);
         }
 
         // 将其他tab的class类获取到，并且判断这个类里是否包含选中类的字段，如果有，将其删除，并返回删除选中类后的类
-        for (let i=1; i < this.length - 1; i++) {
+        for (let i=1; i < _this.length - 1; i++) {
             // 先获取到之前的class类名
-            this.oldClassName = this.eletabArr[i].getAttribute('class');
+            _this.oldClassName = _this.eletabArr[i].getAttribute('class');
             // 如果这个单元里包含选中项class类，则做删除
-            if (this.oldClassName.indexOf(this.activeClass) != -1) {
+            if (_this.oldClassName.indexOf(_this.activeClass) != -1) {
                 // 使用splice删除这个
-                // let classArr = this.oldClassName.split(this.activeClass);
-                this.removeActiveClass(this.eletabArr, i);
+                // let classArr = _this.oldClassName.split(_this.activeClass);
+                _this.removeActiveClass(_this.eletabArr, i);
             }
-            this.eletabArr[i].setAttribute('class', this.oldClassName);
+            _this.eletabArr[i].setAttribute('class', _this.oldClassName);
         }
     }
 
     // 因为第一项已经被修改，所以从第二项开始循环
-    for (let k=1; k < this.length; k++) {
+    for (let k=1; k < _this.length; k++) {
         // 如果循环项大于最大长度，则直接返回
-        if (k > this.length) {
+        if (k > _this.length) {
             return false;
         }
 
         // 如果k等于最后一个下标，则表明是最后一项
-        if (k === (this.length - 1)) {
+        if (k === (_this.length - 1)) {
             // 判断区域的offsetTop值减去tab的高度，并且这个高度小于页面的scrollTop值时，此判断成立
             // 或者滚动高度加上页面可视区域高度等于滚动条的高度时，此判断也成立
-            if (this.areaArr[k].offsetTop - this.fixdTabHeight < this.scrollTop || this.scrollTop + this.clientHeight === this.scrollHeight) {
+            if (_this.areaArr[k].offsetTop - _this.fixdTabHeight < _this.scrollTop || _this.scrollTop + _this.clientHeight === _this.scrollHeight) {
                 // 此时的k应该等于(数组长度-1)，并给此项在原来的基础类的基础上添加选中的class类
-                this.getClass(this.eletabArr[k]);
+                _this.getClass(_this.eletabArr[k]);
                 // 而此时，其他项应该去掉这个选中类activeClass
                 // 因为第k项现在是选中项，那么a不等于k的项，就是要去掉activeClass类的项，这里我们先获取此项的原始类，并且判断里面是否含有activeClass类，如果有，删除此类
-                this.removeActiveClass(this.eletabArr, k);
+                _this.removeActiveClass(_this.eletabArr, k);
             };
             break;
         }
         else {
-            // 判断区域的offsetTop值减去tab的高度，并且这个高度小于页面的scrollTop值并且下一项的offsetTop值减去tab高大于this.scrollTopshi，此判断成立
-            if (this.areaArr[k].offsetTop - this.fixdTabHeight < this.scrollTop && this.areaArr[k + 1].offsetTop - this.fixdTabHeight > this.scrollTop) {
+            // 判断区域的offsetTop值减去tab的高度，并且这个高度小于页面的scrollTop值并且下一项的offsetTop值减去tab高大于_this.scrollTopshi，此判断成立
+            if (_this.areaArr[k].offsetTop - _this.fixdTabHeight < _this.scrollTop && _this.areaArr[k + 1].offsetTop - _this.fixdTabHeight > _this.scrollTop) {
                 // 此时的k应该等于(数组长度-1)，并给此项在原来的基础类的基础上添加选中的class类
-                this.getClass(this.eletabArr[k]);
+                _this.getClass(_this.eletabArr[k]);
                 // 而此时，其他项应该去掉这个选中类activeClass
                 // 因为第k项现在是选中项，那么a不等于k的项，就是要去掉activeClass类的项，这里我们先获取此项的原始类，并且判断里面是否含有activeClass类，如果有，删除此类
-                this.removeActiveClass(this.eletabArr, k);
+                _this.removeActiveClass(_this.eletabArr, k);
             }
         }
     }
